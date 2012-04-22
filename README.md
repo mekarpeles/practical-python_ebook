d75874ca4013e148f8ddc5311ca6ea77a3f02a14
Michael E. Karpeles <mek@baybo.it>
2012-04-21 22:28:59 -0400

    Updated the README, added an auto-reloader (which really should only periodically reload if debug is enabled). Created templates for the ebook website and setup the app with a wsgi.py file so it can run on dotcloud

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 practical-python_ebook
 ======================
+
+<h1>Practical Python</h1>
+
+<p><em>Practical Python</em> is published by Hyperink under a <a href="http://creativecommons.org/licenses/by/3.0/">free license</a></p>
+
+<p>The goal of this book is to improve one's Py-foo by presenting patterns, strategies, techniques, and best practice for solving common problems efficiently in Python.</p>
+
---
+++ wsgi.py
@@ -0,0 +1,21 @@
+import web
+from reloader import PeriodicReloader
+
+urls = ("/", "Index",
+        "/([Pp]ractical-[Pp]ython)/?", "Ebook")
+
+application = web.application(urls, globals())
+render = web.template.render('templates/')
+
+class Index:
+    def GET(self, book=None):
+        return render.template()
+
+class Ebook:
+    def GET(self, title):
+        book = getattr(self, 'title', 'default').lower()
+                   
+
+if __name__ == "__main__":
+    PeriodicReloader()
+    application.run()
