f40be483275a86edc6dac86674a9e0f2d8fa9400
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-05 23:36:49 -0700

    Migrated main.py to waltz, updated html accordingly

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
-import web
-from reloader import PeriodicReloader
+#-*- coding: utf-8 -*-
 
-urls = ("/", "Index",
-        "/(.*)-chapter-([0-9]+)/?", "Chapter",
-        "/([Pp]ractical-[Pp]ython)/chapters/([0-9]+)/?", "Chapter")
+"""
+    main.py
+    ~~~~~~~
 
-app = web.application(urls, globals())
-slender = web.template.render('templates/', globals={})
-render = web.template.render('templates/', base="book")
+    Application for Practical Python Book
 
-class Index:
-    def GET(self, book=None):
-        """
-        kwargs should be moved to books.cfg or there should be a
-        book_<title>.cfg per book or [title] within books.cfg
-        """
-        kwargs = {"title": "Practical Python",
-                  "subtitle": "A Guide to Better Python",
-                  "version": "v.0.0.1",
-                  "description": "By Mek; Hyperink 2012, ISBN 9781614642411. Practical Python is the fastest way to learn python tricks.",
-                  "keywords": "Python,Practical Python,programming,hacks,learning",
-                  }                  
-        return slender.template(**kwargs)
+    :copyright: (c) 2012 Mek.
+    :license: GPLv3 , see LICENSE for more details.
+"""
 
-class Chapter:
-    def GET(self, title, chapter=0):
-        try:
-            return getattr(render.chapters, 'chapter_%s' % chapter, 'chapter_1')()
-        except:
-            return "coming soon"
+import waltz
+
+urls = ("/", "routes.home.Index",
+        "/([Pp]ractical-[Pp]ython)/chapters/([0-9]+)/?", "routes.chapters.Chapter")
+
+env = {}
+app = waltz.setup.dancefloor(urls, globals(), env=env)
 
 if __name__ == "__main__":
-    PeriodicReloader()
     app.run()
-else:
-    web.config.debug = False
-    application = app.wsgifunc()
